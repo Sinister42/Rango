9962bd04f947dfd2e0da27625a69ccdf9a5389d6
Petr Home <sramekpeta@seznam.cz>
2017-01-18 17:15:40 +0000

    Directories media, static, templates moved to the BASE_DIR, settings.py corrected accordingly

--- rango_django/settings.py
+++ rango_django/settings.py
@@ -14,9 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
-STATIC_DIR = os.path.join(os.path.dirname(__file__), 'static')
-MEDIA_DIR = os.path.join(os.path.dirname(__file__), 'media')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
